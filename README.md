2af9343245ab0f50177d1633a7c09a6f36479ce8
tedshd <tedshd@gmail.com>
2024-11-16 17:17:56 +0800

    [dev] Update code.

--- README.md
+++ README.md
@@ -10,24 +10,94 @@ only support jpg / jpeg / png
 
 get image info from preupload file
 
-FileList(FileList): FileList object from HTML input file
+* FileList(FileList): FileList object from HTML input file
 
 ### resizeImage(File, size)
 
 resize image resolution
 
-File(File): a image file object that want to resize
+* File(File): a image file object that want to resize
 
-size(Int): resized size
+* size(Int): resized size
 
-return
+this function return
 
-resizedImage(Blob): resized image binary object
+* resizedImage(Blob): resized image binary object
 
-canvas(canvas): this canvas show resized image
+* canvas(canvas): this canvas show resized image
 
 ### newImageFile(image)
 
 resized image want to upload, use this function handle image to FileList
 
 image(Blob|Blob Array): Blob object from `resizedImage`
+
+this function return
+
+* FileList(FileList): FileList object
+
+### getInmage(imageUrl)
+
+use Fetch api get image to handle canvas image CORS policy
+
+* imageUrl(String): image url
+
+this function return
+
+* image base64 string
+
+### base64ToImageBlob(base64, mime)
+
+trans image base64 string to blob for upload image
+
+* base64(String): image base64 string
+
+* mime: image mime type, ex: `image/jpeg`, `image/png`
+
+this function return
+
+* image blob
+
+### uploadImage({ imageBlob, url, accessToken })
+
+this function is sample for upload image
+
+```javascript
+{
+  imageBlob,
+  url,
+  accessToken
+}
+```
+
+* imageBlob(Blob): image
+
+* url: upload image api url
+
+* accessToken: accessToken
+
+this function return
+
+* api response
+
+### base64ToFilePNG(base64, filename)
+
+trans image base64 to png
+
+this function like base64ToImageBlob
+
+* base64(String): image base64 string
+
+* filename(String): file name
+
+this function return
+
+* image blob
+
+### downloadImage(imageBlob, filename)
+
+download image
+
+* imageBlob(Blob): image
+
+* filename(String): download file
--- js_image_handler.js
+++ js_image_handler.js
@@ -53,7 +53,7 @@
       return
     }
 
-    function dataURLToBlob (dataURL) {
+    function dataURLToBlob(dataURL) {
       let BASE64_MARKER = ';base64,'
       if (dataURL.indexOf(BASE64_MARKER) == -1) {
         let parts = dataURL.split(',')
@@ -109,11 +109,12 @@
         canvas.getContext('2d').drawImage(image, 0, 0, width, height)
         let dataUrl = canvas.toDataURL('image/png')
         let resizedImage = dataURLToBlob(dataUrl)
-        resolve({resizedImage, canvas})
+        resolve({ resizedImage, canvas })
       }
     })
   }
 
+  // 產生新的 File 物件
   const newImageFile = (images) => {
     const dataTransfer = new DataTransfer()
 
@@ -135,7 +136,100 @@
     return dataTransfer.files
   }
 
+  // 取得圖片的 Base64 字符串
+  const getInmage = async (imageUrl) => {
+    try {
+      const response = await fetch(imageUrl);
+      const blob = await response.blob();
+
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onloadend = () => resolve(reader.result); // 當 FileReader 完成後，結果就是 Base64 編碼
+        reader.onerror = reject; // 處理可能的錯誤
+        reader.readAsDataURL(blob); // 將 Blob 轉換為 Base64 字符串
+      });
+    } catch (error) {
+      console.error('get image error:', error);
+      throw error;
+    }
+  }
+
+  // 將 Base64 字符串轉換為 Blob, 以便上傳
+  const base64ToImageBlob = (base64, mime) => {
+    const byteString = atob(base64.split(',')[1]); // 將 base64 的數據部分解碼
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ab], { type: mime });
+  }
+
+  const uploadImage = async ({ imageBlob, url, accessToken }) => {
+    const formData = new FormData();
+    formData.append('file', imageBlob);
+    const response = await fetch(url, {
+      method: 'POST',
+      body: formData,
+      headers: {
+        'x-access-token': accessToken
+      }
+    });
+    const data = await response.json();
+    return data;
+  };
+
+  const base64ToFilePNG = async (base64, filename) => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = base64;
+
+      img.onload = () => {
+        const canvas = document.createElement('canvas');
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        const ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0);
+
+        canvas.toBlob((blob) => {
+          if (blob) {
+            const file = new File([blob], filename, { type: 'image/png' });
+            resolve(file);
+          } else {
+            console.error('Canvas to blob failed');
+            reject(new Error('Canvas to blob failed'));
+          }
+        }, 'image/png');
+      };
+
+      img.onerror = (err) => {
+        reject(err);
+      };
+    });
+  }
+
+  const downloadImage = (imageBlob, filename) => {
+    const url = URL.createObjectURL(imageBlob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.innerText = 'download'
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+  }
+
   window.getImagesInfo = getImagesInfo
   window.resizeImage = resizeImage
   window.newImageFile = newImageFile
+  window.getInmage = getInmage
+  window.base64ToImageBlob = base64ToImageBlob
+  window.uploadImage = uploadImage
+  window.base64ToFilePNG = base64ToFilePNG
+  window.downloadImage = downloadImage
 })()
